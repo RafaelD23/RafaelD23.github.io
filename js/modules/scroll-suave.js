06c9f977edcbb63bc46e8554aa47411514e78db6
Rafael Duarte <duarterafaelmm@gmail.com>
2021-12-10 10:25:09 -0300

    primeiro commit

---
+++ js/modules/scroll-suave.js
@@ -0,0 +1,30 @@
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+  }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
+    });
+  }
+
+  init() {
+    this.addLinkEvent();
+  }
+}
