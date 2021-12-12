b2818391d67b1602e8f18704bd2d4fff30efb556
Rafael Duarte <duarterafaelmm@gmail.com>
2021-12-12 15:56:02 -0300

    anima scroll refatorado

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,20 +1,46 @@
-export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('[data-anime="scroll"]');
-  const windowMetade = window.innerHeight * 0.6;
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
 
-  function animaScroll() {
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - windowMetade) < 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  // Pega a distância de cada item em relação
+  // ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
-  if (sections.length) {
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
+
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o event de scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
