acba49322df755a4368d2df8dd3dabf6bdd031e2
Rafael Duarte <duarterafaelmm@gmail.com>
2021-12-12 18:13:28 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
 import MenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import ScrollAnima from "./modules/scroll-animacao.js";
@@ -39,6 +39,9 @@ const dropdownMenu = new DropdownMenu("[data-dropdown]");
 dropdownMenu.init();
 
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
+funcionamento.init();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 
