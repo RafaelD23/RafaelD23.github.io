f1d347314bc6f2f63ac22e2777acff1fe8183395
Rafael Duarte <duarterafaelmm@gmail.com>
2021-12-12 18:21:10 -0300

    caminho numero certos

--- js/script.js
+++ js/script.js
@@ -43,6 +43,6 @@ const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
